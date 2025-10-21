75a32989477224643a869a811785780ad940c417
Veigar <rafa.veiga@hotmail.com>
2025-10-21 08:00:59 -0300

    Build fixed commited on build-optimized.sh

--- build-optimized.sh
+++ build-optimized.sh
@@ -2,13 +2,13 @@
 set -e
 
 echo "🔄 Cleanup completo..."
-#docker-compose down -v --rmi all 2>/dev/null || true
-#docker system prune -a -f
+docker-compose down -v --rmi all 2>/dev/null || true
+docker system prune -a -f
 
 echo "🏗️ Build otimizado..."
-docker-compose build #--no-cache --parallel
+docker-compose build --no-cache --parallel
 
 
-#python generate_compose.py
+python generate_compose.py
 
 docker-compose -f docker-compose.generated.yml up
